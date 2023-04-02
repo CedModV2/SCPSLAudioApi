5d8317ae61c3841a432e3ab679e9d71b03d0d618
FakeMan2332 <mystrunic32@gmail.com>
2023-04-02 16:24:40 +0500

    pizdec split

---
+++ SCPSLAudioApi/Api/AudioPlayerBase.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using MEC;
+using SCPSLAudioApi.AudioCore;
+using VoiceChat;
+
+namespace SCPSLAudioApi.Api
+{
+    public class AudioPlayerBase
+    {
+        public static Dictionary<ReferenceHub, AudioPlayerBase> AudioPlayers;
+
+        private readonly AudioPlayerComponent _component;
+
+        /// <summary>
+        ///     If URLs are allowed to be played
+        /// </summary>
+        public bool AllowUrl;
+
+        /// <summary>
+        ///     List of Paths/Urls that the player will play from (Urls only work if <see cref="AllowUrl" /> is true)
+        /// </summary>
+        public List<string> AudioToPlay = new List<string>();
+
+        /// <summary>
+        ///     If not empty, the audio will only be sent to players with the PlayerIds in this list
+        /// </summary>
+        public List<int> BroadcastTo = new List<int>();
+
+        /// <summary>
+        ///     Whether the Player should continue playing by itself after the current Track ends.
+        /// </summary>
+        public bool Continue = true;
+
+        /// <summary>
+        ///     Path/Url of the currently playing audio file.
+        /// </summary>
+        public string CurrentPlay;
+
+        /// <summary>
+        ///     Stream containing the Audio data
+        /// </summary>
+        public MemoryStream CurrentPlayStream;
+
+        /// <summary>
+        ///     Boolean indicating whether or not the Queue will loop (Audio will be added to the end of the queue after it gets
+        ///     removed on play)
+        /// </summary>
+        public bool Loop;
+
+        /// <summary>
+        ///     Whether the Player should be sending audio to the broadcaster.
+        /// </summary>
+        public bool ShouldPlay = true;
+
+        /// <summary>
+        ///     If the playlist should be shuffled when an audio track start.
+        /// </summary>
+        public bool Shuffle;
+
+        /// <summary>
+        ///     If Vebose logs shouldbe shown (Note: can be very spammy)
+        /// </summary>
+        public bool VerboseLogs;
+
+        static AudioPlayerBase()
+        {
+            AudioPlayers = new Dictionary<ReferenceHub, AudioPlayerBase>();
+        }
+
+        public AudioPlayerBase(ReferenceHub referenceHub, AudioPlayerComponent audioPlayerComponent)
+        {
+            ReferenceHub = referenceHub;
+            _component = audioPlayerComponent;
+        }
+
+        /// <summary>
+        ///     The ReferenceHub instance that this player sends as.
+        /// </summary>
+        public ReferenceHub ReferenceHub { get; }
+
+        /// <summary>
+        ///     Volume that the player will play at.
+        /// </summary>
+        public float Volume { get; set; } = 100f;
+
+        /// <summary>
+        ///     Gets or Sets the Channel where audio will be played in
+        /// </summary>
+        public VoiceChatChannel BroadcastChannel { get; set; } = VoiceChatChannel.Proximity;
+
+        /// <summary>
+        ///     Add or retrieve the AudioPlayerBase instance based on a ReferenceHub instance.
+        /// </summary>
+        /// <param name="hub">The ReferenceHub instance that this AudioPlayer belongs to</param>
+        /// <returns>
+        ///     <see cref="AudioPlayerBase" />
+        /// </returns>
+        public static AudioPlayerBase Get(ReferenceHub hub)
+        {
+            if (AudioPlayers.TryGetValue(hub, out var player)) return player;
+
+            var audioPlayerComponent = hub.gameObject.AddComponent<AudioPlayerComponent>();
+            player = new AudioPlayerBase(hub, audioPlayerComponent);
+
+            AudioPlayers.Add(hub, player);
+            return player;
+        }
+
+        /// <summary>
+        ///     Start playing audio, if called while audio is already playing the player will skip to the next file.
+        /// </summary>
+        /// <param name="queuePos">The position in the queue of the audio that should be played.</param>
+        public virtual void Play(int queuePos)
+        {
+            if (_component.PlaybackCoroutine.IsRunning)
+                Timing.KillCoroutines(_component.PlaybackCoroutine);
+
+            _component.PlaybackCoroutine = Timing.RunCoroutine(_component.Playback(queuePos), Segment.FixedUpdate);
+        }
+
+        /// <summary>
+        ///     Stops playing the current Track, or stops the player entirely if Clear is true.
+        /// </summary>
+        /// <param name="clear">If true the player will stop and the queue will be cleared.</param>
+        public virtual void Stoptrack(bool clear)
+        {
+            if (clear) AudioToPlay.Clear();
+
+            _component.stopTrack = true;
+        }
+
+        /// <summary>
+        ///     Add an audio file to the queue
+        /// </summary>
+        /// <param name="audio">Path/Url to an audio file (Url only works if <see cref="AllowUrl" /> is true)</param>
+        /// <param name="pos">Position that the audio file should be inserted at, use -1 to insert at the end of the queue.</param>
+        public virtual void Enqueue(string audio, int pos)
+        {
+            if (pos == -1)
+                AudioToPlay.Add(audio);
+            else
+                AudioToPlay.Insert(pos, audio);
+        }
+    }
+}
---
+++ SCPSLAudioApi/AudioCore/AudioPlayerComponent.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Exiled.API.Features;
+using MEC;
+using NVorbis;
+using SCPSLAudioApi.Api;
+using SCPSLAudioApi.Events.Handlers;
+using UnityEngine;
+using UnityEngine.Networking;
+using VoiceChat;
+using VoiceChat.Codec;
+using VoiceChat.Codec.Enums;
+using VoiceChat.Networking;
+using Random = UnityEngine.Random;
+
+namespace SCPSLAudioApi.AudioCore
+{
+    public class AudioPlayerComponent : MonoBehaviour
+    {
+        public AudioPlayerBase AudioPlayerBase { get; private set; }
+
+        public void Start()
+        {
+            AudioPlayerBase = AudioPlayerBase.Get(ReferenceHub.GetHub(this));
+        }
+
+        public virtual void Update()
+        {
+            if (!ready)
+                return;
+            
+            if (AudioPlayerBase.ReferenceHub == null)
+                return;
+
+            if (StreamBuffer.Count == 0 || !AudioPlayerBase.ShouldPlay)
+                return;
+
+            allowedSamples += Time.deltaTime * samplesPerSecond;
+            var toCopy = Mathf.Min(Mathf.FloorToInt(allowedSamples), StreamBuffer.Count);
+
+            if (AudioPlayerBase.VerboseLogs)
+                Log.Info($"1 {toCopy} {allowedSamples} {samplesPerSecond} " +
+                         $"{StreamBuffer.Count} {PlaybackBuffer.Length} {PlaybackBuffer.WriteHead}");
+
+            if (toCopy > 0)
+                for (var i = 0; i < toCopy; i++)
+                    PlaybackBuffer.Write(StreamBuffer.Dequeue() * (AudioPlayerBase.Volume / 100f));
+
+            if (AudioPlayerBase.VerboseLogs)
+                Log.Info($"2 {toCopy} {allowedSamples} {samplesPerSecond} " +
+                         $"{StreamBuffer.Count} {PlaybackBuffer.Length} {PlaybackBuffer.WriteHead}");
+
+            allowedSamples -= toCopy;
+
+            while (PlaybackBuffer.Length >= 480)
+            {
+                PlaybackBuffer.ReadTo(SendBuffer, 480);
+                var dataLen = Encoder.Encode(SendBuffer, EncodedBuffer);
+
+                foreach (var plr in ReferenceHub.AllHubs)
+                {
+                    if (plr.connectionToClient == null ||
+                        (AudioPlayerBase.BroadcastTo.Count > 0 && !AudioPlayerBase.BroadcastTo.Contains(plr.PlayerId)))
+                        continue;
+
+                    plr.connectionToClient.Send(new VoiceMessage(AudioPlayerBase.ReferenceHub,
+                        AudioPlayerBase.BroadcastChannel,
+                        EncodedBuffer, dataLen, false));
+                }
+            }
+        }
+
+        public virtual void OnDestroy()
+        {
+            if (PlaybackCoroutine.IsValid)
+                Timing.KillCoroutines(PlaybackCoroutine);
+
+            AudioPlayerBase.AudioPlayers.Remove(AudioPlayerBase.ReferenceHub);
+        }
+
+        internal virtual IEnumerator<float> Playback(int position)
+        {
+            stopTrack = false;
+            var index = position;
+
+            Track.InvokeTrackSelectingEvent(AudioPlayerBase, index == -1, ref index);
+
+            if (index != -1)
+            {
+                if (AudioPlayerBase.Shuffle)
+                    AudioPlayerBase.AudioToPlay = AudioPlayerBase.AudioToPlay.OrderBy(_ => Random.value).ToList();
+
+                AudioPlayerBase.CurrentPlay = AudioPlayerBase.AudioToPlay[index];
+                AudioPlayerBase.AudioToPlay.RemoveAt(index);
+
+                if (AudioPlayerBase.Loop)
+                    AudioPlayerBase.AudioToPlay.Add(AudioPlayerBase.CurrentPlay);
+            }
+
+            Track.InvokeTrackSelectedEvent(AudioPlayerBase, index == -1, index, ref AudioPlayerBase.CurrentPlay);
+            Log.Debug("Loading Audio");
+
+            if (AudioPlayerBase.AllowUrl &&
+                Uri.TryCreate(AudioPlayerBase.CurrentPlay, UriKind.Absolute, out var result))
+            {
+                var webRequest = new UnityWebRequest(AudioPlayerBase.CurrentPlay, "GET");
+                var downloadHandler = new DownloadHandlerBuffer();
+                webRequest.downloadHandler = downloadHandler;
+
+                yield return Timing.WaitUntilDone(webRequest.SendWebRequest());
+
+                if (webRequest.responseCode != 200)
+                {
+                    Log.Error($"Failed to retrieve audio {webRequest.responseCode} {webRequest.downloadHandler.text}");
+
+                    if (!AudioPlayerBase.Continue || AudioPlayerBase.AudioToPlay.Count < 1) yield break;
+
+                    yield return Timing.WaitForSeconds(1);
+
+                    if (AudioPlayerBase.AudioToPlay.Count >= 1)
+                        Timing.RunCoroutine(Playback(0));
+
+                    yield break;
+                }
+
+                AudioPlayerBase.CurrentPlayStream = new MemoryStream(webRequest.downloadHandler.data);
+            }
+            else
+            {
+                if (File.Exists(AudioPlayerBase.CurrentPlay))
+                {
+                    if (!AudioPlayerBase.CurrentPlay.EndsWith(".ogg"))
+                    {
+                        Log.Error(
+                            $"Audio file {AudioPlayerBase.CurrentPlay} is not valid. Audio files must be ogg files");
+                        yield return Timing.WaitForSeconds(1);
+
+                        if (AudioPlayerBase.AudioToPlay.Count >= 1)
+                            Timing.RunCoroutine(Playback(0));
+
+                        yield break;
+                    }
+
+                    AudioPlayerBase.CurrentPlayStream =
+                        new MemoryStream(File.ReadAllBytes(AudioPlayerBase.CurrentPlay));
+                }
+                else
+                {
+                    Log.Error($"Audio file {AudioPlayerBase.CurrentPlay} does not exist. skipping.");
+                    yield return Timing.WaitForSeconds(1);
+
+                    if (AudioPlayerBase.AudioToPlay.Count >= 1)
+                        Timing.RunCoroutine(Playback(0));
+
+                    yield break;
+                }
+            }
+
+            AudioPlayerBase.CurrentPlayStream.Seek(0, SeekOrigin.Begin);
+            VorbisReader = new VorbisReader(AudioPlayerBase.CurrentPlayStream);
+
+            if (VorbisReader.Channels >= 2)
+            {
+                Log.Error($"Audio file {AudioPlayerBase.CurrentPlay} is not valid. Audio files must be mono.");
+                yield return Timing.WaitForSeconds(1);
+
+                if (AudioPlayerBase.AudioToPlay.Count >= 1)
+                    Timing.RunCoroutine(Playback(0));
+
+                VorbisReader.Dispose();
+                AudioPlayerBase.CurrentPlayStream.Dispose();
+
+                yield break;
+            }
+
+            if (VorbisReader.SampleRate != 48000)
+            {
+                Log.Error(
+                    $"Audio file {AudioPlayerBase.CurrentPlay} is not valid. Audio files must have a SamepleRate of 48000");
+                yield return Timing.WaitForSeconds(1);
+
+                if (AudioPlayerBase.AudioToPlay.Count >= 1)
+                    Timing.RunCoroutine(Playback(0));
+
+                VorbisReader.Dispose();
+                AudioPlayerBase.CurrentPlayStream.Dispose();
+
+                yield break;
+            }
+
+            Track.InvokeTrackLoadedEvent(AudioPlayerBase, index == -1, index, AudioPlayerBase.CurrentPlay);
+            Log.Debug($"Playing {AudioPlayerBase.CurrentPlay} with samplerate of {VorbisReader.SampleRate}");
+
+            samplesPerSecond = VoiceChatSettings.SampleRate * VoiceChatSettings.Channels;
+            //_samplesPerSecond = VorbisReader.Channels * VorbisReader.SampleRate / 5;
+            SendBuffer = new float[samplesPerSecond / 5 + HeadSamples];
+            ReadBuffer = new float[samplesPerSecond / 5 + HeadSamples];
+
+            while (VorbisReader.ReadSamples(ReadBuffer, 0, ReadBuffer.Length) > 0)
+            {
+                if (stopTrack)
+                {
+                    VorbisReader.SeekTo(VorbisReader.TotalSamples - 1);
+                    stopTrack = false;
+                }
+
+                while (!AudioPlayerBase.ShouldPlay) yield return Timing.WaitForOneFrame;
+
+                while (StreamBuffer.Count >= ReadBuffer.Length)
+                {
+                    ready = true;
+                    yield return Timing.WaitForOneFrame;
+                }
+
+                for (var i = 0; i < ReadBuffer.Length; i++) StreamBuffer.Enqueue(ReadBuffer[i]);
+            }
+
+            Log.Debug("Track Complete.");
+
+            var nextQueuePos = 0;
+
+            switch (AudioPlayerBase.Continue)
+            {
+                case true when AudioPlayerBase.Loop && index == -1:
+                    nextQueuePos = -1;
+
+                    Timing.RunCoroutine(Playback(nextQueuePos));
+                    Track.InvokeFinishedTrackEvent(AudioPlayerBase, AudioPlayerBase.CurrentPlay, false,
+                        ref nextQueuePos);
+
+                    yield break;
+                case true when AudioPlayerBase.AudioToPlay.Count >= 1:
+                    Timing.RunCoroutine(Playback(nextQueuePos));
+                    Track.InvokeFinishedTrackEvent(AudioPlayerBase, AudioPlayerBase.CurrentPlay, index == -1,
+                        ref nextQueuePos);
+
+                    yield break;
+                default:
+                    Track.InvokeFinishedTrackEvent(AudioPlayerBase, AudioPlayerBase.CurrentPlay, index == -1,
+                        ref nextQueuePos);
+                    break;
+            }
+        }
+
+        #region Internal
+
+        public const int HeadSamples = 1920;
+
+        public bool stopTrack;
+
+        public bool ready;
+
+        public CoroutineHandle PlaybackCoroutine;
+
+        public int samplesPerSecond;
+
+        public Queue<float> StreamBuffer { get; } = new Queue<float>();
+
+        public VorbisReader VorbisReader { get; set; }
+
+        public float[] SendBuffer { get; set; }
+
+        public float[] ReadBuffer { get; set; }
+
+        public OpusEncoder Encoder { get; } = new OpusEncoder(OpusApplicationType.Voip);
+
+        public PlaybackBuffer PlaybackBuffer { get; } = new PlaybackBuffer();
+
+        public byte[] EncodedBuffer { get; } = new byte[512];
+
+        public float allowedSamples;
+
+        #endregion
+    }
+}
